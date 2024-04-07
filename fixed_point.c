cf7fcde6d00d2087cf711d746174630f228547b1
jouae <51120603+jouae@users.noreply.github.com>
2024-04-08 00:46:16 +0800

    Add a fixed point example
    Solve a root finding problem for function f(x)
    
    The function f(x) = x^3 + 4x^2 - 10 has a root in [1,2], to find root
    by fixed-point iteration the following functions are manipulated from
    f(x):
    
    (1) g1(x) = x - powf(x, 3) - 4*powf(x, 2) + 10
    (2) g2(x) = sqrtf(10/x - 4*x)
    (3) g3(x) = sqrtf(10 - powf(x, 3))/2
    (4) g4(x) = sqrtf(10 / (4 + x))
    (5) g5(x) = x - (powf(x, 3) + 4*powf(x, 2) - 10)/(3*powf(x, 2) + 8*x)
    
    The sequences generated by (1) and (2) are divergent since g1 and g2
    are not contraction mapping with interval [1, 2], the fixed-point is
    not exit in [1, 2].

---
+++ fixed_point.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define MAX_COUNT 10
+
+/* solve f(x) = x^3 + 4x^2 - 10 */
+
+float g1(float x)
+{
+    float p;
+    p = x - powf(x, 3) - 4*powf(x, 2) + 10;
+    return p;
+}
+
+float g2(float x)
+{
+    double p;
+    p = sqrtf(10/x - 4*x);
+    return p;
+}
+
+float g3(float x)
+{
+    double p;
+    p = sqrtf(10 - powf(x, 3))/2;
+    return p;
+}
+
+float g4(float x)
+{
+    double p;
+    p = sqrtf(10 / (4 + x));
+    return p;
+}
+
+float g5(float x)
+{
+    float p;
+    p = x - (powf(x, 3) + 4*powf(x, 2) - 10)/(3*powf(x, 2) + 8*x);
+    return p;
+}
+int main()
+{
+    float tol = 1e-4;
+    float p_pre = 1.5;
+    float p_cur = 1e5;
+    float diff = fabsf(p_pre - p_cur);
+    
+    printf("initial: %f\n", p_pre);
+    for(int i=0; i < MAX_COUNT; i++)
+    {
+        if(diff < tol || isnan(p_cur))
+            break;
+
+        p_cur = g2(p_pre);
+        diff = fabsf(p_pre - p_cur);
+        printf("%d iteration:\n", i+1);
+        printf("    p_pre: %f\n", p_pre);
+        printf("    p_cur: %f\n", p_cur);
+        p_pre = p_cur;
+    }
+
+    return 0;
+}
